4a7420ae0beb480aae345248e0903d72d0b649b5
Sunil Jadhav <45939197+gitsgulabjamun@users.noreply.github.com>
2025-09-14 23:34:02 +0530

    Create _layout.tsx
    Add app root layout

---
+++ app/_layout.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+
+export default function RootLayout() {
+  return (
+    <SafeAreaProvider>
+      <StatusBar style="light" />
+      <Stack>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="onboarding" options={{ headerShown: false }} />
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      </Stack>
+    </SafeAreaProvider>
+  );
+}
